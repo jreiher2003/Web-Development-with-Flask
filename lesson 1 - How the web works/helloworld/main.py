8d6785f07d1b08a7314498c903763193f5c79c00
Jeff Reiher <jeffreiher@gmail.com>
2015-05-29 16:19:26 -0400

    add inline form and add make_response function where if you change html to plain you can see the raw html

--- lesson 1 - How the web works/helloworld/main.py
+++ lesson 1 - How the web works/helloworld/main.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
+form = """
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Worlds!'
+    response = make_response(form)
+    response.headers['Content-Type'] = 'text/plain'
+    return response
 
 
 @app.errorhandler(404)
