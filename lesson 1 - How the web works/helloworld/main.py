debd947a7f272758da2a85eac990e9c93a407220
Jeff Reiher <jeffreiher@gmail.com>
2015-05-29 17:20:21 -0400

    add testhandler which takes data from form and post it to /testhandler in plain text

--- lesson 1 - How the web works/helloworld/main.py
+++ lesson 1 - How the web works/helloworld/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request, redirect, url_for
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 form = """
-<form action="http://www.google.com/search">
-	<input name="q">
+<form action="/testform" method="POST">
+	<input name="name">
 	<input type="submit">
 </form>
 """
@@ -15,8 +15,17 @@ form = """
 def hello():
     """Return a friendly HTTP greeting."""
     response = make_response(form)
-    response.headers['Content-Type'] = 'text/plain'
+    # response.headers['Content-Type'] = 'text/html'
     return response
+
+@app.route('/testform', methods=['GET', 'POST'])
+def TestHandler():
+	if request.method == 'POST':
+		name = request.form['name']
+		response = make_response(name)
+		return response
+	else:
+		return url_for('hello')
 
 
 @app.errorhandler(404)
