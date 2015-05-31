6b3c395caf75322a0076b03164710d28af331787
Jeff Reiher <jeffreiher@gmail.com>
2015-05-31 10:47:24 -0400

    made form2 in app and basic post request birthday validation fun

--- lesson 1 - How the web works/helloworld/main.py
+++ lesson 1 - How the web works/helloworld/main.py
@@ -10,6 +10,25 @@ form = """
 	<input type="submit">
 </form>
 """
+# birthday validation
+form2 = """
+<form action="/birthday" method="POST">
+	What is your birthday?
+	<br>
+	<label>Month
+	<input type="text" name="month">
+	</label>
+	<label>Day
+	<input type="text" name="day">
+	</label>
+	<label>Year
+	<input type="text" name="year">
+	</label>
+	<br>
+	<br>
+	<input type="submit">
+</form>
+"""
 
 @app.route('/')
 def hello():
@@ -17,6 +36,10 @@ def hello():
     response = make_response(form)
     # response.headers['Content-Type'] = 'text/html'
     return response
+
+@app.route('/lesson2')
+def lesson2():
+	return render_template('play.html')
 
 @app.route('/testform', methods=['GET', 'POST'])
 def TestHandler():
@@ -27,9 +50,15 @@ def TestHandler():
 	else:
 		return url_for('hello')
 
-@app.route('/lesson2')
-def lesson2():
-	return render_template('play.html')
+# birthday validation
+@app.route('/birthday', methods=['GET','POST'])
+def birthday():
+	if request.method == 'POST':
+		return "Thanks that is a totally valid day"
+	else:	
+		response = make_response(form2)
+		return response
+
 
 
 @app.errorhandler(404)
