ef61ee4cfc94b278a64dc2baaf8ba387469bf116
Jeff Reiher <jeffreiher@gmail.com>
2015-06-03 14:23:31 -0400

    add valid day function

--- lesson 1 - How the web works/helloworld/utils.py
+++ lesson 1 - How the web works/helloworld/utils.py
@@ -8,9 +8,25 @@ monthdict= dict((m[:3].lower(),m) for m in months)
 def valid_month(month):
 	
 	short_month = month[:3].lower()
+	print short_month
 	if short_month in monthdict.keys():
 		return monthdict.get(short_month) # get method retrieves values from dicts
 
 
-print valid_month('decdfdf')
+# print valid_month('decdfdf')
 ## February
+
+def valid_day(day):
+	if type(day) == int:
+		day = day
+	elif type(day) == str:
+		day = int(day)
+	if day >= 1 and day <= 31:
+		return day
+	else:
+		return None
+
+print valid_day(0)
+print valid_day('1')
+print valid_day(14)
+print valid_day('300')
