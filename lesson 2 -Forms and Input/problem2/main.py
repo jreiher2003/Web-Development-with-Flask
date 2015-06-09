216cb046edb88073b6e6d4bbb8b187da653e6d6c
Jeff Reiher <jeffreiher@gmail.com>
2015-06-09 10:51:23 -0400

    add rot13 function working

--- lesson 2 -Forms and Input/problem2/main.py
+++ lesson 2 -Forms and Input/problem2/main.py
@@ -1,7 +1,7 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -12,6 +12,18 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/rot13', methods=["GET","POST"])
+def rot13():
+	if request.method == "GET":
+		return render_template('rot13.html')
+
+	if request.method == 'POST':
+		rot = request.form['text']
+		if rot:
+			rots = rot.encode('rot13')
+
+			return render_template('rot13.html', rots=rots)
+	
 
 @app.errorhandler(404)
 def page_not_found(e):
