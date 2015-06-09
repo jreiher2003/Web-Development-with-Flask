79f5cd77c1ef6b045b2b582a1221b9c823d562c1
Jeff Reiher <jeffreiher@gmail.com>
2015-06-09 11:43:41 -0400

    fix typo

--- lesson 2 -Forms and Input/problem2/utils.py
+++ lesson 2 -Forms and Input/problem2/utils.py
@@ -1,13 +1,13 @@
 import re
 
-USER_RE = re.complile(r"^[a-zA-Z0-9_-]{3,20}$")
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
 	return USER_RE.match(username)
 
-PASS_RE = re.complile(r"^.{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
 def valid_password(password):
 	return PASS_RE.match(password)
 
-EMAIL_RE = re.complile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+EMAIL_RE = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 def valid_email(email):
 	return EMAIL_RE.match(email)
