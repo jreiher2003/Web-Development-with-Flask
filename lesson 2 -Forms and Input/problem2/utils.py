9dfad021574fa5b0648f29ad1756469c790cf15e
Jeff Reiher <jeffreiher@gmail.com>
2015-06-11 11:11:41 -0400

    fix bug in utils verify code

--- lesson 2 -Forms and Input/problem2/utils.py
+++ lesson 2 -Forms and Input/problem2/utils.py
@@ -2,12 +2,18 @@ import re
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
-	return USER_RE.match(username)
+	username = USER_RE.match(username)
+	if username:
+		return username.group(0)
 
 PASS_RE = re.compile(r"^.{3,20}$")
 def valid_password(password):
-	return PASS_RE.match(password)
+	password = PASS_RE.match(password)
+	if password:
+		return password.group(0)
 
 EMAIL_RE = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 def valid_email(email):
-	return EMAIL_RE.match(email)
+	email = EMAIL_RE.match(email)
+	if email:
+		return email.group(0)
