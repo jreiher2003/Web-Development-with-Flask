57428a5b1fd13122b0240c71cd493427947accb9
Jeff Reiher <jeffreiher@gmail.com>
2015-07-19 11:48:31 -0400

    setup on gae complete

--- lesson 2a -Templates/main.py
+++ lesson 2a -Templates/main.py
@@ -1,4 +1,11 @@
 from flask import Flask 
+import sys
+import os.path
+# add `lib` subdirectory to `sys.path`, so our `main` module can load
+# third-party libraries. as in Virtualenv 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'Lib/site-packages'))
+
+
 app = Flask(__name__)
 
 @app.route("/")
