516e12a2ff49c4e6f751b985f1940e9a6ccde1ff
Jeff Reiher <jeffreiher@gmail.com>
2015-07-20 18:02:54 -0400

    fizzbuzz

--- lesson 2a -Templates/main.py
+++ lesson 2a -Templates/main.py
@@ -1,39 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-form_input = """<form>
-		<h2>Add a Food</h2>
-		<input type="text" name="food">
-		%s
-		<button>Add</button>
-	</form>"""
 
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
 
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-<br><br><h2>Shopping List</h2>
-<ul>%s</ul>"""
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-	output = form_input
-	output_hidden = ""
+	if request.method == 'GET':
+		n = 15
+		return render_template('index.html', n=n)
 
-	items = request.form['food']
-	if items:
-		for item in items:
-			output_hidden += hidden_html % item
-			output_items += item_html % item
-
-		output_shopping = item_html % output_items
-		output += output_shopping
-	output = output % output_hidden
-	return output
+	if request.method == "POST":
+		n = request.form['name']
+		return redirect('hello', n=n)
 
 if __name__ == "__main__":
 	app.run()
