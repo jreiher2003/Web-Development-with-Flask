055b0309da7dca7b9048aedb9d43cac53e849d01
Jeff Reiher <jreiher2003@yahoo.com>
2015-11-28 17:12:29 -0500

    made success and error messages when submitting our form

--- lesson 3 - Databases/asciichan/app.py
+++ lesson 3 - Databases/asciichan/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('front.html')
+	if request.method == 'GET':
+		return render_template('front.html')
+
+	if request.method == 'POST':
+		title = request.form['title']
+		art = request.form['art']
+		error = 'need a title and art'
+		success ="Thanks for submitting"
+		return render_template('front.html', title=title, art=art, error=error, success=success)
 
 if __name__ == '__main__':
     app.run(debug=True)
