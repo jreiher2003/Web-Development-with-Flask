6a8a9fd923301abed11e78ad653925a732b75271
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-02 09:15:59 -0500

    add newpost.html, some bootrap stylesto base and newpost, add some form handling to newpost()

--- lesson 3 - Databases/basicblog/blog/views.py
+++ lesson 3 - Databases/basicblog/blog/views.py
@@ -2,5 +2,16 @@ from flask import Flask, render_template, request, url_for, redirect
 from blog import app 
 
 @app.route('/')
+@app.route('/blog/')
 def index():
 	return render_template("base.html")
+
+@app.route('/blog/newpost/', methods=['GET', 'POST'])
+def newpost():
+	if request.method == 'POST':
+		subject = request.form['subject']
+		blog = request.form['blog']
+		return render_template('base.html')
+
+	if request.method == 'GET':
+		return render_template('newpost.html')
