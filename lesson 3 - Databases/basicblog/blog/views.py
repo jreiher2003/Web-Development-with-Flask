a792cd1ce72239d74b39c7976a60aa358b485dd9
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-02 08:26:34 -0500

    add templates folder and base.html, tested the render_template() function in views, everything works

--- lesson 3 - Databases/basicblog/blog/views.py
+++ lesson 3 - Databases/basicblog/blog/views.py
@@ -1,6 +1,6 @@
-from flask import Flask 
+from flask import Flask, render_template, request, url_for, redirect
 from blog import app 
 
 @app.route('/')
 def index():
-	return "Basic Blog start"
+	return render_template("base.html")
