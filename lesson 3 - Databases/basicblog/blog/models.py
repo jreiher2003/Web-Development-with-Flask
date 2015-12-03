694196be11f8440c93ea5003de8d6c700edd2a8f
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-03 14:20:31 -0500

    figured out the 301 redirect with blog id post in var

--- lesson 3 - Databases/basicblog/blog/models.py
+++ lesson 3 - Databases/basicblog/blog/models.py
@@ -5,5 +5,7 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(80), nullable=False)
     blog = db.Column(db.String, nullable=False)
-    created = db.Column(db.Date)
+    created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                                       onupdate=db.func.current_timestamp())
 
--- lesson 3 - Databases/basicblog/blog/views.py
+++ lesson 3 - Databases/basicblog/blog/views.py
@@ -3,5 +3,26 @@ from blog import app, db
 from models import *
 
 @app.route('/')
+@app.route('/blog/')
 def index():
-	return 'hello'
+	all_posts = db.session.query(Entry).all()
+	return render_template('front.html', all_posts=all_posts)
+
+@app.route('/blog/newpost/', methods=['GET', 'POST'])
+def newpost():
+	if request.method == 'POST':
+		subject = request.form['subject']
+		blog = request.form['blog']
+		newEntry = Entry(subject=subject, blog=blog)
+		db.session.add(newEntry)
+		db.session.commit()
+		last_id = db.session.query(Entry).order_by(Entry.id.desc()).first()
+		return redirect(url_for('permalink', id=last_id.id))
+
+	if request.method == 'GET':
+		return render_template('newpost.html')
+
+@app.route('/blog/<id>/')
+def permalink(id):
+	last_entry = db.session.query(Entry).order_by(Entry.id.desc()).first()
+	return render_template('permalink.html', last_entry=last_entry)
