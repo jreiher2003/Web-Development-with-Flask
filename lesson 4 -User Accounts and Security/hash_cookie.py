257dc544eed71737b32544562342553da7713ddf
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-24 00:32:58 -0500

    add check_secure_val and list comp to check hash vals

--- lesson 4 -User Accounts and Security/hash_cookie.py
+++ lesson 4 -User Accounts and Security/hash_cookie.py
@@ -10,3 +10,19 @@ def make_secure_val(s):
 	return "%s, %s" % (s, hash_str(s))
 
 print make_secure_val('foobar')
+
+
+def check_secure_val(s):
+	HASH = make_secure_val(s)
+	if hash_str(s) == HASH.split(',')[1].strip():
+		return HASH.split(',')[0].strip() 
+	else:
+		return None
+	# print HASH.split(',')[1].strip()
+
+
+print check_secure_val('foobar')
+
+
+HASH = make_secure_val('foobar')
+print [ HASH.split(',')[0].strip() if hash_str('foobar') == HASH.split(',')[1].strip() else None ][0]
