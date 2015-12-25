cba140238d0b3f4fc24b6c81c224b6df100a051d
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-25 12:05:09 -0500

    pep8 styles

--- lesson 4 -User Accounts and Security/hash_cookie.py
+++ lesson 4 -User Accounts and Security/hash_cookie.py
@@ -3,13 +3,10 @@ import hashlib
 def hash_str(s):
 	return hashlib.md5(s).hexdigest()
 
-# print hash_str('foobar')
-
 
 def make_secure_val(s):
 	return "%s, %s" % (s, hash_str(s))
 
-# print make_secure_val('foobar')
 
 
 def check_secure_val(s):
@@ -25,10 +22,9 @@ print check_secure_val('foobar, 3858f62230ac3c915f300c664312c63f')
 
 
 
-
 s = 'foobar, 3858f62230ac3c915f300c664312c63f'
 val = s.split(',')[0].strip()
 hash_val = s.split(',')[1].strip()
 check_val =  make_secure_val(val).split(',')[1].strip()
-# print [ HASH.split(',')[0].strip() if hash_str('foobar') == HASH.split(',')[1].strip() else None ][0]
+
 print [val if hash_val == check_val else None][0]
