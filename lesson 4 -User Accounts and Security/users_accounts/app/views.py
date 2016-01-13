095f953507c61b3798347881f6ac6796c5d4acd9
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-13 10:34:30 -0500

    Update cookie

--- lesson 4 -User Accounts and Security/users_accounts/app/views.py
+++ lesson 4 -User Accounts and Security/users_accounts/app/views.py
@@ -1,8 +1,56 @@
 from app import app, db
-from flask import flash, redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for, session, make_response
 from flask.ext.login import login_user, logout_user, login_required, current_user
 from forms import LoginForm, RegisterForm, MessageForm
 from app.models import User, BlogPost, bcrypt
+import hmac 
+
+
+def hash_str(s):
+    return hmac.new(app.config['SECRET_KEY'], s).hexdigest()
+
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+
+def check_secure_val(h):
+    val = h.split('|')[0].strip()
+    hash_val = h.split('|')[1].strip()
+    check_val =  make_secure_val(val).split('|')[1].strip()
+    if hash_val == check_val:
+        return val
+    else:
+        return None
+
+
+# @app.route('/')
+# def onlyCookie():
+#     response = make_response(redirect(url_for('onlyCookie')))
+#     response.set_cookie('visits', value='0')
+#     visits = request.cookies.get('visits',0)
+#     try:
+#         visits
+#         visits = visits.split('|')[0]
+#         if visits.isdigit():
+#             visits = int(visits) + 1
+#     except AttributeError:
+#          visits = 0
+#     has = make_secure_val(str(visits))
+#     if check_secure_val(has) != None:
+#         if visits < 100:
+#             response = make_response("you have been here %s times!" % visits)
+#             response.set_cookie('visits', value='%s' % has)
+#         else:
+# 			response = make_response("Your awesome")
+# 			response.set_cookie('visits', value='%s' % has)
+#     elif check_secure_val(has) == None:
+#         visits = 0
+#     	response = make_response("cookie value doesn't make the hash now the value is %s" % visits)
+#     	response.set_cookie('visits', value=0)
+    
+#     return response
+
 
 @app.route('/', methods=['GET', 'POST'])
 # @login_required
@@ -38,12 +86,17 @@ def login():
 	return render_template("login.html", form=form, error=error)
 
 
+
+
 @app.route('/logout')
-@login_required
+# @login_required
 def logout():
-	logout_user()
-	flash('You were logged out.', 'bg-danger')
-	return redirect(url_for('index'))
+    # logout_user()
+    session.clear()
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('visits', value='0')
+    flash('You were logged out.', 'bg-danger')
+    return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
