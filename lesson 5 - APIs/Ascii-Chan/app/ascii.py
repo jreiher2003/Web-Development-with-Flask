6e74e48ffa7a9c5890321ac970f898937b87982c
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-26 13:44:56 -0500

    Add geo ip location

--- lesson 5 - APIs/Ascii-Chan/app/ascii.py
+++ lesson 5 - APIs/Ascii-Chan/app/ascii.py
@@ -1,8 +1,23 @@
 import datetime
+import urllib2
+import json
 from app import app, db
 from app.models import AsciiArt
 from flask import render_template, request, url_for, redirect
-  
+
+IP_URL = "http://ip-api.com/json/"
+def get_coords(ip):
+	url = IP_URL + ip
+	content = None
+	content = urllib2.urlopen(url).read()
+	if content:
+		result = json.loads(content)
+		# print result.keys()
+		lon = result['lon']
+		lat = result['lat']
+		return lat, lon
+	else:
+		return None
 
 @app.route("/", methods=["GET","POST"])
 def hello():
