b46f71dc835c08e5939ee061f9ea44012b037f0a
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-27 11:55:17 -0500

    Add list to query

--- lesson 5 - APIs/Ascii-Chan/app/ascii.py
+++ lesson 5 - APIs/Ascii-Chan/app/ascii.py
@@ -18,7 +18,7 @@ def get_coords(ip):
 		return (lat, lon)
 	
 
-GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=580x300&zoom=13&sensor=false&markers=26.876,-82.2681"
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=580x300&zoom=3&sensor=false&markers=26.876,-82.2681&markers=40.7128, -74.0059"
 def gmaps_img():
     # markers = '&'.join('markers=%s,%s' % (p.lat, p.lon) for p in points)
     # return GMAPS_URL+markers
@@ -26,21 +26,21 @@ def gmaps_img():
 
 @app.route("/", methods=["GET","POST"])
 def hello():
-    points = get_coords(request.remote_addr)
+    # points = get_coords(request.remote_addr)
     img_url = gmaps_img()
     
     all_art = AsciiArt.query.order_by(AsciiArt.id.desc()).all()
-    arts = list(all_art)
-    # points = filter(None, (a.coords for a in arts))
+    all_art = list(all_art)
+    points = []
+    for a in all_art:
+        points.append(a.coords)
+    # points = filter(None, (a.coords for a in all_art))
     img_url = None
     img_url = gmaps_img()
 
     if request.method == 'POST':
         coords1 = get_coords(request.remote_addr)
         one = AsciiArt(title=request.form['title'], art=request.form['art'], coords=str(coords1))
-        # if coords1:
-        #     one.coords = coords1
-    	
         db.session.add(one)
         db.session.commit()
         return redirect(url_for('hello', all_art=all_art, img_url=img_url, points=points))
