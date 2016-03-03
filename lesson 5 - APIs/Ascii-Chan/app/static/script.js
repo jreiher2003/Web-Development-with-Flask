56d8044b220c4c7da68f74d15f4844ec73f9f730
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-03 11:49:01 -0500

    Add links to nav add jquery to load street maps

---
+++ lesson 5 - APIs/Ascii-Chan/app/static/script.js
@@ -0,0 +1,31 @@
+function loadData() {
+
+    var $body = $('#body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    var streetStr = $("#street").val()
+    var cityStr = $("#city").val()
+    var address = streetStr + ", " + cityStr;
+
+    $greeting.text("So, you want to live at " + address + "?");
+
+    var streetviewUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x200&location=" + address + "";
+    $body.append("<img class='bgimg center-block' src='"+ streetviewUrl + "'>");
+
+    $.getJSON();
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
+
+// loadData();
