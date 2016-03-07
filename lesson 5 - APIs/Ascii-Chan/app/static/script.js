a35cb9bad0809571511d34fd4b72ba0980ffef55
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-07 10:55:56 -0500

    Add $.ajax() request with wikilinks api

--- lesson 5 - APIs/Ascii-Chan/app/static/script.js
+++ lesson 5 - APIs/Ascii-Chan/app/static/script.js
@@ -11,7 +11,6 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
     var streetStr = $("#street").val()
     var cityStr = $("#city").val()
     var address = streetStr + ", " + cityStr;
@@ -26,23 +25,35 @@ function loadData() {
     var KEY = "&sort=newest&api-key=53ac231dce440f01b73fcdda60cccc59:4:70248560"
     var URL = API + cityStr + KEY
     $.getJSON(URL, function( data ) {
-        // console.log(data)
         $nytHeaderElem.text("New York Times Articles About " + cityStr);
-        console.log(data.response.docs)
         var articles = data.response.docs;
-        console.log(articles.length)
-        // console.log(articles[0]['web_url']);
         for (var i = 0; i < articles.length; i++ ) {
             var article = articles[i];
             $nytElem.append("<li>" + "<a href='"+ article.web_url +"' target='_blank'>" + article.headline.main +"</a>" + "<p>" + article.snippet + "</p></li>");
         };
-        // console.log(URL)
     }).error(function(e) {
         $nytElem.text("Ny Times Couldn't be loaded at this time")
     });
+
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources"); 
+    }, 8000);
+
+    var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityStr + "&format=json&callback=wikiCallback";
+    $.ajax({
+        url: wikiURL,
+        dataType: "jsonp",
+        success: function ( data ) {
+            var articleList = data[1]
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i]
+                var url = "https://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append("<li><a href='" + url + "' target='_blank'>" + articleStr + "</a></li>");
+            }; 
+            clearTimeout(wikiRequestTimeout);
+        }
+    })
     return false;
 };
 
 $('#form-container').submit(loadData);
-
-// loadData();
