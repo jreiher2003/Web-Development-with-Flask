684ceb5c79e7f687b855f45e78a0cb78d84fda89
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-20 02:22:25 -0400

    Tabs to spaces

--- lesson 6 - Caching/cycle_servers.py
+++ lesson 6 - Caching/cycle_servers.py
@@ -4,22 +4,22 @@ from pprint import pprint
 SERVERS = ['SERVER1', 'SERVER2', 'SERVER3', 'SERVER4']
 
 def get_server():
-	cycle_servers = itertools.cycle(SERVERS)
-	i = 0
-	for value in cycle_servers:
-		print value
-		i += 1
-		if i >= 4:
-			break
-	# print cycle_servers.next()
-	# print cycle_servers.next()
-	# print cycle_servers.next()
-	# print cycle_servers.next()
+    cycle_servers = itertools.cycle(SERVERS)
+    i = 0
+    for value in cycle_servers:
+        print value
+        i += 1
+        if i >= 4:
+            break
+    # print cycle_servers.next()
+    # print cycle_servers.next()
+    # print cycle_servers.next()
+    # print cycle_servers.next()
 n = -1
 def get_server_u():
-	global n
-	n += 1
-	return SERVERS[n % len(SERVERS)]
+    global n
+    n += 1
+    return SERVERS[n % len(SERVERS)]
 
         
 
